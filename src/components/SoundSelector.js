2caf6de6aa02f42cf3e77fa711b092a11414bd48
Your Name <mmuzasantuna@gmail.com>
2020-12-09 18:22:58 +0000

    commented out code deleted

--- src/components/SoundSelector.js
+++ src/components/SoundSelector.js
@@ -9,11 +9,6 @@ function SoundSelector(props) {
                 return <option value={sound.url}> {sound.name} </option>
             })}
         </select>
-        
-            {/* <input onChange={handleSoundChange}type="radio" id="wow" name="soundChoice" value="Wow" checked/>
-            <label for="wow">Wow</label>
-            <input type="radio" id="beep" name="soundChoice" value="Beep"/>
-            <label for="beep">Beep</label> */}
         </>
     );
 };
